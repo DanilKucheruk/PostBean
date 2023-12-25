66c522ba1d7ac49b64f067f08f659dd14a73b16d
Danil Kucheruk <danilkucheruk8@gmail.com>
2023-12-25 17:14:02 +0300

    init

---
+++ src/main/java/ru/danil/kucheruk/postbean/PostbeanApplication.java
@@ -0,0 +1,13 @@
+package ru.danil.kucheruk.postbean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostbeanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostbeanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/danil/kucheruk/postbean/PostbeanApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.danil.kucheruk.postbean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostbeanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
